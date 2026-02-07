bb32a2a0c04dfb6fe5a963149e3da6108689124a
chuans <juaazn.05@gmail.com>
2026-02-07 12:25:28 +0100

    Archivos donde se ejecutan las funcionalidades del main

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import { renderizar } from './render';
+
+const contenerdorMain = document.getElementById('mainContenedor');
+const canvasHome = document.getElementById('canvasHome');
+
+function ajustarCanvas() {
+    const anchoContenedor = contenerdorMain.clientWidth;
+    const altoContenedor = contenerdorMain.clientHeight;
+    
+    canvasHome.width = anchoContenedor;
+    canvasHome.height = altoContenedor;
+
+    renderizar();
+} 
+
+window.addEventListener('load', ajustarCanvas);
+window.addEventListener('resize', ajustarCanvas);
