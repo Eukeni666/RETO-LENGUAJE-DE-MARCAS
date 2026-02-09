63a22e7d4d0a6a91c938e0a1496850c76d891ddf
chuans <juaazn.05@gmail.com>
2026-02-09 01:32:23 +0100

    eventos al cargar la pagina

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
-import { renderizar } from './render.js';
+import { ajustarCanvas, escucharEventoClick } from './eventos.js';
 
-const contenerdorMain = document.getElementById('mainContenedor');
-const canvasHome = document.getElementById('canvasHome');
+window.addEventListener('load', () => {
+    ajustarCanvas();
+    escucharEventoClick();
+});
 
-function ajustarCanvas() {
-    const anchoContenedor = contenerdorMain.clientWidth;
-    const altoContenedor = contenerdorMain.clientHeight;
-    
-    canvasHome.width = anchoContenedor;
-    canvasHome.height = altoContenedor;
-
-    renderizar();
-} 
-
-window.addEventListener('load', ajustarCanvas);
 window.addEventListener('resize', ajustarCanvas);
