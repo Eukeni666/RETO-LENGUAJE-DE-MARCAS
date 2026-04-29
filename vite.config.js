1c190281d2e0c0c24fc18cc74be55e62eeb67bab
Juan Diego Acosta Liscano <juaazn.05@gmail.com>
2026-04-29 20:11:53 +0200

    Configuracion servidor

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
 
 export default defineConfig({
+  base: '/RETO-LENGUAJE-DE-MARCAS/',
   build: {
     rollupOptions: {
       input: {
