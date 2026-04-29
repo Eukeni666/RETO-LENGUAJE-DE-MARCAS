76eede4943910ad20f675a7a5a73c1815b87d978
Chuans_ <90198371+juaazn@users.noreply.github.com>
2026-04-29 12:08:57 +0200

    Rutas

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        baseDeDatos: resolve(__dirname, 'src/pages/baseDeDatos.html'),
+        digitalizacion: resolve(__dirname, 'src/pages/digitalizacion.html'),
+        entornos: resolve(__dirname, 'src/pages/entornos.html'),
+        ingles: resolve(__dirname, 'src/pages/ingles.html'),
+        ipe: resolve(__dirname, 'src/pages/ipe.html'),
+        lenguajeDeMarcas: resolve(__dirname, 'src/pages/lenguaje-de-marcas.html'),
+        sostenibilidad: resolve(__dirname, 'src/pages/sostenibilidad.html')
+      }
+    }
+  }
+})
